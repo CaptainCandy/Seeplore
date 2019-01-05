b9c20a14e7f249eb95fcffaabc52b07833c76aec
CaptainCandy <33003223+CaptainCandy@users.noreply.github.com>
2019-01-05 15:48:24 +0800

    一些“我的”界面和申请界面的小修改

--- miniprogram/pages/mine/myApply.js
+++ miniprogram/pages/mine/myApply.js
@@ -21,9 +21,26 @@ Page({
     utils.getMyApplication().then(
       res => {
         if (res.isSubmitted) {
+          res.data.map(apply => {
+            //控制时间的展示样式，当天的帖子显示小时分钟，非当天的显示日期
+            let now = new Date();
+            let createTime = new Date(apply.createTime);
+            if (now.getFullYear() == createTime.getFullYear() && now.getDate() == createTime.getDate() && now.getMonth() == createTime.getMonth()) {
+              let strTime = null;
+              if (createTime.getMinutes() <= 9 && createTime.getMinutes() >= 0) strTime = createTime.getHours() + ':0' + createTime.getMinutes();
+              else strTime = createTime.getHours() + ':' + createTime.getMinutes();
+              createTime = strTime;
+            }
+            else {
+              let strTime = createTime.getFullYear() + '-' + (createTime.getMonth() + 1) + '-' + createTime.getDate();
+              createTime = strTime;
+            }
+            apply.createTime = createTime;
+          })
           that.setData({
-            applyList: res.data
+            applyList: res.data.reverse()
           })// 用户申请表的数据，状态字段包括：isChecked, isApproved, message(管理员check时附上的消息)
+          console.log(res.data)
         } else {
           //TODO 用户未曾提交过申请
           that.setData({
