7f6b5ef70b0843d7b7657374c37881302d237d8f
CaptainCandy <33003223+CaptainCandy@users.noreply.github.com>
2019-01-19 18:47:11 +0800

    修改一些小细节

--- miniprogram/pages/mine/userSite.js
+++ miniprogram/pages/mine/userSite.js
@@ -1,5 +1,6 @@
 // miniprogram/pages/mine/userSite.js
 const app = getApp()
+const utils = require('../../utils/utils.js')
 
 Page({
 
@@ -10,6 +11,7 @@ Page({
     userInfo: null, //使用者
     siteUser: null, //看的页面属于哪个用户
     isMine: false,
+    isFollow: false, //当前用户是否已经关注该页面用户
   },
 
   /**
@@ -26,6 +28,15 @@ Page({
       this.setData({
         siteUser: options.targetUserid
       })
+      utils.checkRelationshipWith(this.data.siteUser).then(res => {
+        // res = {myFollowing: false, 当前用户未关注目标用户
+        //        myFollower: false} 当前用户未被目标用户关注
+        if (res.myFollowing) {
+          that.setData({
+            isFollow: true,
+          })
+        }
+      });
     }
     wx.cloud.callFunction({
       name: 'getUserInfo',
@@ -134,5 +145,93 @@ Page({
     wx.navigateTo({
       url: 'myPost?isMine' + this.data.isMine + '&targetUserid=' + this.data.siteUser,
     })
+  },
+
+  onMyFollow: function () {
+    wx.navigateTo({
+      url: 'follower?isFollow=true&userid=' + this.data.siteUser,
+    })
+  },
+
+  onMyFollower: function () {
+    wx.navigateTo({
+      url: 'follower?isFollow=false&userid=' + this.data.siteUser,
+    })
+  },
+
+  onFollow: function () {
+    let that = this
+    let isFollow = this.data.isFollow
+    if (isFollow) {
+      wx.showLoading({
+        title: '正在取消',
+      })
+      utils.unfollowTargetUser(this.data.siteUser).then(res => {
+        // res.ok == true; 取消成功时
+        // res.notFollowing == true; 本来就没关注
+        if (res.ok) {
+          wx.hideLoading()
+          wx.showToast({
+            title: '取消成功！',
+          })
+          that.setData({
+            isFollow: false
+          })
+        }
+        else {
+          wx.hideLoading()
+          if (res.notFollowing) {
+            wx.showToast({
+              title: '您本来就没有关注这个用户，请稍后尝试重新进入页面！',
+              icon: "none",
+              duration: 2000
+            })
+          }
+          else{
+            wx.showToast({
+              title: '取关失败，请稍后再试！',
+              icon: "none",
+              duration: 2000
+            })
+          }
+        }
+      });
+    }
+    else {
+      wx.showLoading({
+        title: '正在关注',
+      })
+      //添加关注
+      utils.followTargetUser(this.data.siteUser).then(res => {
+        // res.ok == true; 关注成功时
+        // res.alreadyFollowing == true; 关注失败时
+        if (res.ok) {
+          wx.hideLoading()
+          wx.showToast({
+            title: '关注成功！',
+          })
+          that.setData({
+            isFollow: true
+          })
+        }
+        else {
+          wx.hideLoading()
+          if (res.alreadyFollowing) {
+            wx.showToast({
+              title: '您已经关注了这个用户，请稍后重新进入页面！',
+              icon: "none",
+              duration: 2000
+            })
+          }
+          else {
+            wx.showToast({
+              title: '关注失败，请稍后再试！',
+              icon: "none",
+              duration: 2000
+            })
+          }
+        }
+      });// 写入数据库失败直接抛出错误。
+    }
   }
 })
